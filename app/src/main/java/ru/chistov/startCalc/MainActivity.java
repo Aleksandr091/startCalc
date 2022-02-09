f2aa01ebaaec5000e1768330030874a349a0d715
Aleksandr091 <Chist675@gmail.com>
2022-02-10 00:53:47 +0300

    Initial commit

---
+++ app/src/main/java/ru/chistov/startCalc/MainActivity.java
@@ -0,0 +1,26 @@
+package ru.chistov.startCalc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn_calc = findViewById(R.id.btn_calc);
+        btn_calc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("CALCULATOR");
+                startActivity(intent);
+            }
+        });
+
+    }
+}
